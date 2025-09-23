4829e3eaf78718a8ced7a5c3395052020c27481e
Mohamed Sakith <Sakithmohamed54@icloud.com>
2025-09-23 17:43:56 +0530

    updated default properties

--- src/models/rentDocument.model.js
+++ src/models/rentDocument.model.js
@@ -2,9 +2,17 @@ const mongoose = require("mongoose");
 
 const rentDocumentSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  documentType: { type: String, required: true },
-  documentUrl: { type: String, required: true },
-  verifiedStatus: { type: Boolean, default: false },
+
+  // Default document type is 'license'
+  documentType: { type: String, default: "license", required: true },
+
+  documentverifiedStatus: { type: Boolean, default: false },
+
+  // Default license field (path can be empty initially)
+  documents: {
+    license: { type: String, default: "" },
+  }
+
 }, { timestamps: true });
 
 module.exports = mongoose.model("RentDocument", rentDocumentSchema);

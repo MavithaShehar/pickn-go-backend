e0946f86796bb9bf2b00d6ea729d03308989d35b
Mohotti5353 <168615486+Mohotti5353@users.noreply.github.com>
2025-11-12 11:25:01 +0530

    update app.js

--- src/app.js
+++ src/app.js
@@ -35,7 +35,8 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(cors());
-app.use(helmet());
+
+app.use(helmet({ crossOriginResourcePolicy: { policy: "cross-origin" } }));
 app.use(morgan("dev"));
 
 // ------------------ Routes ------------------
